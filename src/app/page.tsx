8c1d094c03fa2782221a4983a76cda12f82077bc
Alana Oliveira <alanaoliveira71@gmail.com>
2024-05-26 05:53:31 -0300

    feat/home: align hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,19 @@
 import LinkWithIcon from "@/components/LinkWithIcon";
-import Arrow from "../../public/arrow.png"
-
+import Arrow from "../../public/arrow.png";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex items-center justify-center min-h-screen">
       <div className="absolute inset-0 bg-cover bg-center z-0"
         style={{ backgroundImage: 'url("/bg-home.png")' }}></div>
-      <div className="relative z-10 container mx-auto px-4">
-        <div className="flex flex-col items-center justify-center">
+      <div className="relative z-10 container mx-auto px-4 text-center">
+        <div>
           <h1 className="text-9xl font-unbounded font-light">
             <span className="font-semibold">D</span>aily <span className="font-semibold">T</span>rivia
           </h1>
-          <div className="py-8">
-            <LinkWithIcon href="/" text="Start Quiz" icon={Arrow} />          </div>
+          <div className="py-8 flex justify-center">
+            <LinkWithIcon href="/" text="Start Quiz" icon={Arrow} />
+          </div>
         </div>
       </div>
     </main>
