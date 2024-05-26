f8d8bd186b2eede75da8c93743d64be44f3b8c74
Alana Oliveira <alanaoliveira71@gmail.com>
2024-05-26 07:03:01 -0300

    feat/home: align hero (responsive view)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,44 @@
+"use client";
+
+import React, { useState, useEffect } from 'react';
+import Image from 'next/image';
 import LinkWithIcon from "@/components/LinkWithIcon";
 import Arrow from "../../public/arrow.png";
+import bgHomeDesktop from "../../public/bg-home.png";
+import bgHomeMobile from "../../public/bg-home-mobile.png";
 
 export default function Home() {
+  const [isMobile, setIsMobile] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 800);
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
-    <main className="flex items-center justify-center min-h-screen">
-      <div className="absolute inset-0 bg-cover bg-center z-0"
-        style={{ backgroundImage: 'url("/bg-home.png")' }}></div>
+    <main className="min-h-screen flex items-center justify-center">
+      <div className="absolute inset-0 w-full h-full ">
+        <Image
+          src={isMobile ? bgHomeMobile : bgHomeDesktop}
+          alt="Background"
+          layout="fill"
+        />
+      </div>
       <div className="relative z-10 container mx-auto px-4 text-center">
-        <div>
-          <h1 className="text-9xl font-unbounded font-light">
+        <div className={isMobile ? "flex flex-col -mt-20" : ""}>
+          <h1 className={isMobile ? "text-7xl font-unbounded font-light leading-snug" : "text-9xl font-unbounded font-light"}>
             <span className="font-semibold">D</span>aily <span className="font-semibold">T</span>rivia
           </h1>
-          <div className="py-8 flex justify-center">
+          <div className={`flex justify-center  ${isMobile ? "" : "py-8"}`}>
             <LinkWithIcon href="/" text="Start Quiz" icon={Arrow} />
           </div>
         </div>
